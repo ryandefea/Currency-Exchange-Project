f8e24e53edcc2c80f06916511dccce35d5124bf3
ryandefea <ryan.defea@gmail.com>
2022-07-01 14:09:30 -0700

    import files from old vscode.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import $ from 'jquery';
+
+import CurrencyService from './currency-service.js';
+
+
+const service = new CurrencyService();
+//function getCurrencyInfo(country, amount) {
+
+//}
+
+
+
+async function makeApiCall(country, amount) {
+  const response = await service.getCurrency(country, amount);
+  //getCurrencyInfo(response);
+  console.log(response);
+}
+
+$(document).ready(function() {
+  $('#moneySubmit').click(function () {
+    let amount = parseInt($('#amount').val());
+    let country = $('#country').val();
+    makeApiCall(country, amount);
+  })
+})
